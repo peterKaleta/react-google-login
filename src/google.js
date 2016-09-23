89f022f349c75e8c1c85f4b57c7f1ce896ca1d0a
Peter Kaleta <ext.pkaleta@riotgames.com>
2016-09-23 16:04:44 +0200

    no scripts, no problems

--- src/google.js
+++ src/google.js
@@ -40,7 +40,11 @@ class GoogleLogin extends Component {
       js = d.createElement(s);
       js.id = id;
       js.src = '//apis.google.com/js/client:platform.js';
-      fjs.parentNode.insertBefore(js, fjs);
+      if(fjs){
+        fjs.parentNode.insertBefore(js, fjs);
+      } else {
+        document.head.appendChild(js);
+      }
       js.onload = cb;
     })(document, 'script', 'google-login', () => {
       const params = {
